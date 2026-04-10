07a1f4aef301d5b2e58be2a1fdf3d5f1c485854b
TO2\pe603380 <petr.kucera@o2.sk>
2026-04-10 21:13:00 +0200

    Odstranění nepoužívaného UpdateProfileRequest DTO

--- app/src/main/java/com/example/memotrip_kroniq/data/remote/dto/UpdateProfileRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.memotrip_kroniq.data.remote.dto;
-
-data class UpdateProfileRequest(
-    val name: String?,
-    val gender: String?,
-    val firstName: String?,
-    val lastName: String?,
-    val dateOfBirth: String?
-)
